d1717b6d0f869329a6dd5670da849a9510a8e137
Lenka Křivánková <lenka.krivankova@email.cz>
2016-04-30 23:32:05 +0200

    Ornstein-Uhlenbeckuv proces
    - graf Ornstein-Uhlenbeckuv procesu i pro více trajektorií včetně
    legendy
    - pokus o vytvoření funkce, která ale nefunguje
    ------------------------------------
    - Geometrický Wienerův proces jako model ceny rizokového aktiva ve
    srovnání s modelem bezrizikového aktiva

--- stoch_proces.R
+++ stoch_proces.R
@@ -53,7 +53,7 @@ points3D (W1[1:100], W2[1:100], W3[1:100], col = "red", phi = 20, theta = 20, xl
 
 generuj.gBp <- function (t, dt, X0, r, sigma) {
   dW <- rnorm (length (t) - 1) * sqrt (dt)
-  dX <- 1 + r * dt + sigma * dW
+  dX <- 1 + r * dt + sigma * dW   #dX/X=r*dt+sigma*dW => X_{i+1}=X_i+dX=X_i+r*dt*X_i+sigma*dW*X_i=X_i*(1+r*dt+sigma*dW)
   X <- cumprod (c (X0 , dX))
 }
 
@@ -120,3 +120,89 @@ for (i in 2:length (r)){        #cyklus
 matplot (t, M, type = "l", lty = 1, xlab = "t", ylab = "S(t)")
 legend(0,max(c(M[,])), c(expression(mu==0~a~sigma==0.1),expression(mu==0~a~sigma==0.5),expression(mu==1~a~sigma==0.1),expression(mu==1~a~sigma==0.5)), col=1:length (r), lty=1,  box.lty=0)
 
+# =============================================================================
+# GWP + bezrizikové aktivum
+
+X0 <- 1#10
+dt <- 0.001
+r <- 1#0
+sigma <- 0.1#1
+t <- seq (0, 1, by = dt)
+
+M <- generuj.gBp (t, dt, X0, r, sigma)
+M<-cbind(exp(r*t),M)
+matplot (t, M, type = "l", lty = 1, xlab = "t", ylab = "S(t)")#"geometricky Brownuv pohyb")
+#abline (h = X0 , lty = 2)
+legend(0,max(c(M[,])),c(expression(e^{r[f]*t}),expression(S(t))), col=1:length (M), lty=1,  box.lty=0)
+# =============================================================================
+# =============================================================================
+# Ornstein-Uhlenbeckuv proces 
+#dr=kappa*(theta-ri)*dt+sigma*dW => r_{i+1}=r_i+dr=r_i+kappa*(theta-r_i)*dt+sigma*dW=r_i*(1-kappa*dt)+kappa*theta*dt+sigma*dW
+
+generuj.OU <- function (t, dt, r0, theta, sigma, kappa) {
+  ri<-r0
+  X<-r0
+  dw_r<-rnorm((length (t) - 1),0,sqrt(dt))    
+  for (i in 1:(length (t) - 1)){  
+    dr<-kappa*(theta-ri)*dt+sigma*dw_r[i] 
+    ri<-ri+dr
+    X<-cbind(X,ri)
+  }
+}
+# =============================================================================
+
+r0<- 2
+dt <- 0.001
+theta <- 2
+sigma <- 0.1
+kappa <- 1
+t <- seq (0, 1, by = dt)
+
+X <- generuj.OU (t, dt, r0, theta, sigma, kappa)
+
+plot (t, X, type="l", col = "red", xlab = "t", ylab = "X(t)")#"geometricky Brownuv pohyb")
+ 
+# =============================================================================
+
+r0<- 2
+dt <- 0.001
+theta <- 2
+sigma <- 0.1
+kappa <- 1
+t <- seq (0, 1, by = dt)
+
+ri<-r0
+X<-r0
+dw_r<-rnorm((length (t) - 1),0,sqrt(dt))    
+for (i in 1:(length (t) - 1)){  
+  dr<-kappa*(theta-ri)*dt+sigma*dw_r[i] 
+  ri<-ri+dr
+  X<-cbind(X,ri)
+}
+
+plot (t, X, type="l", col = "red", xlab = "t", ylab = "X(t)")#"geometricky Brownuv pohyb")
+abline (h = theta , lty = 2)  
+
+# =============================================================================
+# vice trajektorii
+
+r0<- 2
+dt <- 0.001
+theta <- 2
+sigma <- 0.1
+kappa <- 1
+t <- seq (0, 1, by = dt)
+M<-t(X)
+for (k in 1:4){
+  ri<-r0
+  X<-r0
+  dw_r<-rnorm((length (t) - 1),0,sqrt(dt))    
+  for (i in 1:(length (t) - 1)){  
+    dr<-kappa*(theta-ri)*dt+sigma*dw_r[i] 
+    ri<-ri+dr
+    X<-cbind(X,ri)}
+M<-cbind(M,t(X))
+}
+
+matplot (t, M, type = "l", lty = 1, xlab = "t", ylab = "X(t)")#"geometricky Brownuv pohyb")
+abline (h = theta , lty = 2)  
